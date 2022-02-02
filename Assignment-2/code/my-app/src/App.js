5491faea559793088e97d60d8e939f84113210f2
Rilwamhaq <rilwanhaq@gmail.com>
2022-02-02 15:35:12 +0530

    Assignment-2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -5,10 +5,18 @@ import './App.css';
 
 function App(props) {
  const {employee}=props;
-    
 return(
   <div className="container">
   {/* Start your React code here */}
+  <img src ={employee.profileImg} alt="profile"/>
+  <h1>{employee.name}</h1>
+  <p>Location</p>
+  <h3>{employee.location}</h3>
+  <p>BloodGroup</p>
+  <h3>{employee.bloodGroup}</h3>
+  <p>age</p>
+  <h3>{employee.age}</h3>
+
 </div>
 )
 }
